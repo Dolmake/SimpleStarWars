180f17e226cd488058b5f1ee1a313c6b3b251041
Dolmake <demondolmake@hotmail.com>
2015-04-29 19:05:00 +0200

    First real commit

---
+++ SimpleStarWars/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SimpleStarWars
+//
+//  Created by Daniel on 29/04/15.
+//  Copyright (c) 2015 Dolmake. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SimpleStarWars/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SimpleStarWars
+//
+//  Created by Daniel on 29/04/15.
+//  Copyright (c) 2015 Dolmake. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
