345169550fb7d39b9f59d597f306d83f40bbd718
Dolmake <demondolmake@hotmail.com>
2015-04-29 19:15:00 +0200

    Table view Controller created. Autolayout disabled

---
+++ SimpleStarWars/DLMKStarWarsCharacterTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  DLMKStarWarsCharacterTableViewController.h
+//  SimpleStarWars
+//
+//  Created by Daniel on 29/04/15.
+//  Copyright (c) 2015 Dolmake. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DLMKStarWarsCharacterTableViewController : UITableViewController
+
+@end
